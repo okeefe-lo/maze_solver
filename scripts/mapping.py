c0a4bcc86d5d66756808685dd21ba4736d339dc6
Ubuntu <ubuntu@ip-172-31-32-88.ec2.internal>
2020-12-09 21:45:03 +0000

    Task 2

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -4,8 +4,9 @@
 import sys
 import rospy
 import numpy as np
+import time
 from math import pi
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 # from tf.transformations import euler_from_quaternion
@@ -48,9 +49,9 @@ def controller():
     # laser_num = laser_params['laser_num']
 
     # Create Sub and Pub
-    pub = rospy.Publisher('/' + sys.argv[1] + '/cmd_vel', Twist, queue_size=1)
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=0)
 
-    rospy.wait_for_message('/' + sys.argv[1] + "/scan", LaserScan)
+    rospy.wait_for_message("/scan", LaserScan)
 
     try:
         # Set rate and intitilize publisher type
@@ -68,6 +69,13 @@ def controller():
         start_pose = current_pose
         desired_pose = current_pose
 
+        last_dis = 0
+        start_pose2 = current_pose
+
+        first_encounter = current_pose
+        start_time = 0
+        spawn_pose = current_pose
+
         status = "Finding Wall"
         directions = np.array([0, pi/2, pi, -pi/2, -pi])
 
@@ -76,6 +84,9 @@ def controller():
 
         # Ideal forward velocity
         d_fwd = .05
+
+        # Front clearence
+        clearence = .17
 
         while not rospy.is_shutdown():
 
@@ -87,56 +98,96 @@ def controller():
                 diff = diff + 2 * pi 
             if abs(diff) > .05:
                 status = "TURNING"
+
+            if abs(ranges[90] - last_dis) > .25:
+                start_pose2 = current_pose
+                print("Moved Past Wall")
             
             # Choose what directions to go
             if status == "Finding Wall":
                 new_fwd_vel = d_fwd
                 print("Looking for Wall")
-                if ranges[0] < .18:
+                if min(ranges[0], ranges[1], ranges[359]) < clearence:
                     status = "TURNING"
                     new_fwd_vel = 0
                     new_fwd_vel = 0
                     desired_pose[2] = current_pose[2] - pi/2
                     desired_pose[2] = find_nearest(directions, desired_pose[2])
+                    first_encounter = current_pose
+                    start_time = time.time()
                     print("Attaching")
                     print("Turn RIGHT")
             elif status == "FORWARD":
                 new_fwd_vel = d_fwd
-                if ranges[0] < .18:
+                if min(ranges[0], ranges[1], ranges[359]) < clearence:
                     status = "IDENTIFY"
                     new_fwd_vel = 0
                     print("Hitting Wall")
-                elif euclidean(current_pose, start_pose) > .3:
+                elif euclidean(current_pose, start_pose) > .26:
                     status = "IDENTIFY"
                     new_fwd_vel = 0
                     new_ang_vel = 0
-                    print("Moved .3 meters")
+                    print("Moved .26 meters")
+                elif abs(ranges[90] - last_dis) > .15:
+                    start_pose2 = current_pose
+                    print("Moved Past Wall")
                 print("Moving FORWARD")
             elif status == "TURNING":
                 new_fwd_vel = 0
-                if abs(desired_pose[2] - current_pose[2]) < .01:
+                if abs(diff) < .01:
                     new_fwd_vel = d_fwd
                     new_ang_vel = 0
                     start_pose = [current_pose[0], current_pose[1]]
                     status = "FORWARD"
                 print("Turning")
+            elif status == "SPAWN":
+                diff = desired_pose[2] - current_pose[2]
+                if diff >= pi:
+                    diff = diff - 2 * pi
+                if diff < -pi:
+                    diff = diff + 2 * pi 
+                if abs(diff) > .05:
+                    new_fwd_vel = 0
+                    if abs(diff) < .01:
+                        new_fwd_vel = d_fwd
+                        new_ang_vel = 0
+                        start_pose = [current_pose[0], current_pose[1]]
+                elif (euclidean(current_pose,first_encounter) <.2): 
+                    print("ARRIVED")
+                    new_fwd_vel = 0
+                else:
+                    new_fwd_vel = d_fwd
             else:
-                if (ranges[90] > .30) & (ranges[125] > .2): #For .2 dis from wall use [120] and .2
+                if (min(ranges[90], ranges[89], ranges[91]) > .30) and (ranges[125] > .18) and (euclidean(current_pose, start_pose2) > .08):
+                    print("HERES WHY  ", min(ranges[90], ranges[89], ranges[91]), "   ", ranges[125], "  ", euclidean(current_pose, start_pose2))
+                    #For .2 dis from wall use [120] and .2, for .18 use [25] and .2
                     status == "TURNING"
                     new_fwd_vel = 0
                     desired_pose[2] = current_pose[2] + pi/2
+                    if desired_pose[2] > pi:
+                        desired_pose[2] = desired_pose[2] - 2 * pi
+                    if desired_pose[2] < -pi:
+                        desired_pose[2] = desired_pose[2] + 2 * pi 
                     desired_pose[2] = find_nearest(directions, desired_pose[2])
                     diff = desired_pose[2] - current_pose[2]
                     print("Turn LEFT")
-                elif ranges[0] < .18:
+                elif min(ranges[0], ranges[1], ranges[359]) < clearence:
+                    print("HERES WHY  ", min(ranges[90], ranges[89], ranges[91]), "   ", ranges[125], "  ", euclidean(current_pose, start_pose2))
                     status == "TURNING"
                     new_fwd_vel = 0
                     desired_pose[2] = current_pose[2] - pi/2
+                    if desired_pose[2] > pi:
+                        desired_pose[2] = desired_pose[2] - 2 * pi
+                    if desired_pose[2] < -pi:
+                        desired_pose[2] = desired_pose[2] + 2 * pi 
                     desired_pose[2] = find_nearest(directions, desired_pose[2])
                     diff = desired_pose[2] - current_pose[2]
                     print("Turn RIGHT")
                 else:
                     new_fwd_vel = d_fwd
+                    if abs(ranges[90] - last_dis) > .15:
+                        start_pose2 = current_pose
+                        print("Moved Past Wall")
                     print("Default: Forward")
 
             diff = desired_pose[2] - current_pose[2]
@@ -146,7 +197,15 @@ def controller():
                 diff = diff + 2 * pi 
             new_ang_vel = k * diff
 
-            print(current_pose[2], "  ",desired_pose[2],"  ", new_ang_vel )
+            if (start_time - time.time() > 30) and (euclidean(current_pose,first_encounter) <.2):
+                spawn = "TRUE"    
+                desired_pose[2] = current_pose[2] - pi/2
+                if desired_pose[2] > pi:
+                    desired_pose[2] = desired_pose[2] - 2 * pi
+                if desired_pose[2] < -pi:
+                    desired_pose[2] = desired_pose[2] + 2 * pi 
+                desired_pose[2] = find_nearest(directions, desired_pose[2]) 
+
 
             # Set data to publish
             data.linear.x = new_fwd_vel
@@ -161,6 +220,7 @@ def controller():
 
             old_fwd_vel = new_fwd_vel
             old_ang_vel = new_ang_vel
+            last_dis = ranges[90]
 
             rate.sleep()
 
@@ -171,7 +231,7 @@ if __name__ == '__main__':
 
     rospy.init_node("Mapping", anonymous=False)
 
-    rospy.Subscriber('/' + sys.argv[1] + "/scan", LaserScan, laser_callback)
-    rospy.Subscriber('/' + sys.argv[1] + "/odom", Odometry, odom_callback)
+    rospy.Subscriber("/scan", LaserScan, laser_callback)
+    rospy.Subscriber("/odom", Odometry, odom_callback)
         
     controller()
--- scripts/navigation.py
+++ scripts/navigation.py
@@ -4,18 +4,95 @@
 import sys
 import rospy
 import time
-from geometry_msgs.msg import Point, PoseStamped
+from math import pi
+from geometry_msgs.msg import Point, PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
+from squaternion import Quaternion
 
 def target_callback(data):
 
+    # To send movement command from command line
+    # rostopic pub /target geometry_msgs/Point '{x: -0.3, y: -2.0}'
+    print("MESSAGE RECEIVED")
+
+    global target_pose
+
     pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
-    target = Point()
+    target = PoseStamped()
     target.pose.position.x = data.x
     target.pose.position.y = data.y
+    target.pose.orientation.x = 0
+    target.pose.orientation.y = 0
+    target.pose.orientation.z = 0
+    target.pose.orientation.w = 1
+    target.header.frame_id = "map"
+    
+    target_pose = [data.x, data.y]
+
+    pub.publish(target)
+    time.sleep(1)
+    pub.publish(target)
+
+def odom_callback(data):
+    """Updates position data"""
+
+    global current_pose
+    q = Quaternion(data.pose.pose.orientation.x, \
+        data.pose.pose.orientation.y, \
+        data.pose.pose.orientation.z, \
+        data.pose.pose.orientation.w)
+    euler = q.to_euler(degrees=False)
+    current_pose = [data.pose.pose.position.x, data.pose.pose.position.y, euler[0]]
+
+def euclidean(current, start):
+    return (((current[0]-start[0]) ** 2) + ((current[1]-start[1]) ** 2)) ** .5
+
+def spawn(initial_pose):
+
+    global target_pose
+
+    pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+    target = PoseStamped()
+    target.pose.position.x = initial_pose[0]
+    target.pose.position.y = initial_pose[1]
+    target.pose.orientation.x = 0
+    target.pose.orientation.y = 0
+    target.pose.orientation.z = 0
+    target.pose.orientation.w = 1
+    target.header.frame_id = "map"
+
+    target_pose = [initial_pose[0], initial_pose[1]]
+
     pub.publish(target)
 
 if __name__ == '__main__':
 
     rospy.init_node("Navigation", anonymous=False)
     rospy.Subscriber("/target", Point, target_callback)
+    rospy.Subscriber("/odom", Odometry, odom_callback)
 
+    rospy.wait_for_message("/odom", Odometry)
+
+    try:
+        global target_pose
+        global current_pose
+
+        target_pose = [1000, 1000]
+
+        spawn_pose = current_pose
+
+        while not rospy.is_shutdown():
+            
+            if (euclidean(current_pose, target_pose) < .1):
+                start_time = time.time()
+                time.sleep(6)
+                spawn(spawn_pose)
+                time.sleep(1)
+                spawn(spawn_pose)
+
+
+        rospy.spin()
+
+
+    except rospy.ROSInterruptException:
+        pass
