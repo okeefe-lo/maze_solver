754531141fca6ed0bb66c8e920994852048052e1
Ubuntu <ubuntu@ip-172-31-32-88.ec2.internal>
2020-12-14 21:41:20 +0000

    Following Updates

--- scripts/rescued.py
+++ scripts/rescued.py
@@ -12,7 +12,6 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from squaternion import Quaternion
 
-
 def communicate(send):
 
     pub = rospy.Publisher("/comm", String, queue_size=0)
@@ -32,9 +31,68 @@ def comm_callback(data):
     # Do something here
         pass
 
+def laser_callback(data):
+    """Updates laser scan data"""
+
+    global ranges
+    ranges = data.ranges
+
+    global old_ranges
+    if len(old_ranges) < 2:
+        old_ranges = ranges
+
+    global robot_angle
+
+    global detected_status 
+    if detected_status == "Undetected":
+        for i in range(360):
+            if abs(ranges[i] - old_ranges[i]) > .1:
+                detected_status = "Detected"
+                robot_angle = i
+
+    old_ranges = ranges
+
 if __name__ == '__main__':
 
     rospy.init_node("Rescued", anonymous=False)
 
-    rospy.Subscriber("/comm", String, comm_callback)
+    try:
 
+        global detected_status
+        detected_status = "Undetected"
+
+        global old_ranges
+        old_ranges = np.empty(1) 
+        
+        rospy.Subscriber("/comm", String, comm_callback)
+        rospy.Subscriber("/" + str(sys.argv[1]) + "/scan", LaserScan, laser_callback)
+
+        pub = rospy.Publisher("/" + str(sys.argv[1]) + "/cmd_vel", Twist, queue_size=0)
+        rate = rospy.Rate(20)
+
+        data = Twist()
+
+        while not rospy.is_shutdown():
+
+            if detected_status == "Undetected":
+                new_fwd_vel = 0
+                new_ang_vel = 0
+            else:
+                pass
+
+            # Set data to publish
+            data.linear.x = new_fwd_vel
+            data.linear.y = 0
+            data.linear.z = 0
+            data.angular.x = 0
+            data.angular.y = 0
+            data.angular.z = new_ang_vel
+
+            # Publish data
+            pub.publish(data)
+
+            rate.sleep()
+
+
+    except rospy.ROSInterruptException:
+        pass
